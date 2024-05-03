04f8f11a2e8ca2ea1f3b895810d93fb1c2a7eb24
lydia-l3 <122927604+lydia-l3@users.noreply.github.com>
2024-05-03 18:36:46 +0000

    first commit - created functions for methods

--- app/models/director.rb
+++ app/models/director.rb
@@ -11,4 +11,11 @@
 #  updated_at :datetime         not null
 #
 class Director < ApplicationRecord
+  def filmography
+    my_id = self.id
+
+    matching_movies = Movie.where({ :director_id => my.id })
+
+    return matching_movies
+  end
 end
